2c333e2ab2350aff99b7d840c012246b6729c1bb
林倚農 <jakeusgay@Mac.lan>
2025-02-18 21:57:53 -0500

    Implemented pydantic BaseModel

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
 
+# Pydantic is meant to 
+# simplify the authentication of API request
 app = FastAPI()
 
+class Item(BaseModel):
+    name:str
+    price:float
+    is_offer:Union[bool, None] = None
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+# Union is a type hint in Python typing module, used to 
+# express that parameter can be of many different types 
 @app.get("/items/{item_id}")
-def read_item(item_id : int, q : Union[str, None] = None):
-    return {"item_id" : item_id, "q" : q}
+def read_item(item_id : int, item_name : Union[str, None] = None):
+    return {"item_id" : item_id, "item name" : item_name}
 
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id, "item_price":item.price}
